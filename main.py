486ca85b9aa06a6c7d04f8ecfabac64c996b813b
shighafuae-cpu <shighafuae@gmail.com>
2026-02-02 19:17:16 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-import numpy as np 
-import tensorflow as tf
-from tensorflow.keras.datasets import imdb
-from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import load_model
-import streamlit as st
+# import numpy as np 
+# import tensorflow as tf
+# from tensorflow.keras.datasets import imdb
+# from tensorflow.keras.preprocessing import sequence
+# from tensorflow.keras.models import load_model
+# import streamlit as st
 
-word_index = imdb.get_word_index()
-reversed_word_index = {value: key for key, value in word_index.items()}
+# word_index = imdb.get_word_index()
+# reversed_word_index = {value: key for key, value in word_index.items()}
 
-model = load_model('simple_rnn_imdb.h5')
+# model = load_model('simple_rnn_imdb.h5')
 
-# step 2: Helper functions
-# function to decode reviews
-def decode_review(encoded_review):
-    return ' '.join([reverse_word_index.get(i -3, '?') for i in encoded_review])
+# # step 2: Helper functions
+# # function to decode reviews
+# def decode_review(encoded_review):
+#     return ' '.join([reverse_word_index.get(i -3, '?') for i in encoded_review])
 
-# function to preprocess user input
-def preprocess_text(text):
-    words = text.lower().split()
-    encoded_review = [word_index.get(word, 2) + 3 for word in words]
-    padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
-    return padded_review
+# # function to preprocess user input
+# def preprocess_text(text):
+#     words = text.lower().split()
+#     encoded_review = [word_index.get(word, 2) + 3 for word in words]
+#     padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
+#     return padded_review
 
-# def predict_sentiment(review):
-#     preproc_sent = preprocess_text(review)
-#     prediction = model.predict(preproc_sent)
-#     sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
-#     return sentiment, prediction[0][0]
+# # def predict_sentiment(review):
+# #     preproc_sent = preprocess_text(review)
+# #     prediction = model.predict(preproc_sent)
+# #     sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+# #     return sentiment, prediction[0][0]
 
 
-## Stremlit app
-st.title('Imdb Movie review Sentiment Analysis')
-st.write('Enter a movie review to classify it as p or n.')
+# ## Stremlit app
+# st.title('Imdb Movie review Sentiment Analysis')
+# st.write('Enter a movie review to classify it as p or n.')
 
-user_input = st.text_area('Movie Review')
-# if st.button('Classify'):
-#     preprocess_value = preprocess_text(user_input)
-#     predict = model.predict(preprocess_value)
-#     st.write(predict)
+# user_input = st.text_area('Movie Review')
+# # if st.button('Classify'):
+# #     preprocess_value = preprocess_text(user_input)
+# #     predict = model.predict(preprocess_value)
+# #     st.write(predict)
 
-# else:
+# # else:
 st.write('Please enter a movie review.')
 
